4e06292d67028ed8cfe00a091ec2c17bf86b67da
sLee4354 <35707828+sLee4354@users.noreply.github.com>
2018-02-14 19:34:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,84 @@
-﻿// Your code here!
-// Get the modal
+﻿
+//register popup
+var modal_register = document.getElementById('register');
+//register button
+var btn_register = document.getElementById('btn_register');
+//register popup
+var modal_login = document.getElementById('login');
+//register button
+var btn_login = document.getElementById('btn_login');
+//register 
+var id = null;
+var pw = null;
+var cpw = null;
+var email = null;
+//go main
+window.onclick = function (event) {
+    if (event.target == modal_register || event.target == modal_login) {
+        modal_register.style.display = "none";
+        btn_register.style.display = 'block';
+        modal_login.style.display = "none";
+        btn_login.style.display = 'block';
+    }
+}
+//register popup display
+function registerModal() {
+    modal_register.style.display = 'block'
+    btn_register.style.display = 'none';
+}
+//login popup display
 function loginModal() {
-    var modal_login = document.getElementById('login');
-    var btn_login = document.getElementById('btn_login');
     modal_login.style.display = 'block'
     btn_login.style.display = 'none';
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal_login) {
-            modal_login.style.display = "none";
-            btn_login.style.display = 'block';
-        }
+}
+
+function registerSubmit() {
+    id = document.getElementById("register_username").value;
+    pw = document.getElementById("register_password").value;
+    cpw = document.getElementById("register_confirmpassword").value;
+    email = document.getElementById("register_email").value;
+
+    if (id == "" || pw == "") {
+        alert("Please complete write your information.");
     }
+    else if (pw != cpw) {
+        alert("your password and confirm password are not matching!");
+    }
+    else {
+        alert("Thanks to join us!");
+        modal_register.style.display = 'none';
+        modal_login.style.display = 'block';
+    }
+    return false;
 }
 
 function loginSubmit() {
-    var id = document.getElementById("username").value;
-    var pw = document.getElementById("password").value;
-    if (id == "1" && pw == "1") {
-        alert(id + ", you are login now!");
+    var loginId = document.getElementById("username").value;
+    var loginPw = document.getElementById("password").value;
+    if (loginId == id && loginPw == pw) {
+        alert(loginId + ", you are login now!");
         window.location = "dashboard.html";
         
-    } else if (id == "1" && pw != "1") {
+    } else if (loginId == id && loginPw != pw) {
         alert("wrong password. Try again!");
     } else {
         alert("No exist username. Please join us!");
     }
     return false;
+}
+//add transaction
+function addSubmit() {
+    var addPopup = document.getElementById('add');
+    alert("Add Success!");
+    addPopup.style.display = "none";
+    return false;
+}
+function addModal() {
+    var addPopup = document.getElementById('add');
+    add.style.display = 'block'
+    window.onclick = function (event) {
+        if (event.target == addPopup) {
+            addPopup.style.display = "none";
+        }
+    }
 }
